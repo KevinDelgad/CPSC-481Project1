70493bd3511538475bfaaef256cc35b70e7a3cf2
Kevin Delgado <kevindel891@gmail.com>
2023-09-21 18:30:23 -0700

    Add starter files

---
+++ wolfgoatcabbage.py
@@ -0,0 +1,9 @@
+from search import *
+# YOUR CODE GOES HERE
+ 
+if __name__ == '__main__':
+    wgc = WolfGoatCabbage()
+    solution = depth_first_graph_search(wgc).solution()
+    print(solution)
+    solution = breadth_first_graph_search(wgc).solution()
+    print(solution)
