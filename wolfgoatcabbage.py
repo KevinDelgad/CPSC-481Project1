a089a6791931c83a9cb915ad6cfefb55f2a39952
Kevin Delgado <kevindel891@gmail.com>
2023-09-28 14:48:19 -0700

    Fix inital errors and change leftBank

--- wolfgoatcabbage.py
+++ wolfgoatcabbage.py
@@ -2,20 +2,16 @@ from search import *
 # YOUR CODE GOES HERE
 
 class WolfGoatCabbage(Problem):
-    def __init__(self, inital = {'F', 'W', 'G', 'C'}, goal = {}):
+    def __init__(self, inital = ('F', 'W', 'G', 'C'), goal = ()):
         super().__init__(inital, goal)
 
     def leftBankActions(self, state, validActions):
-        returnActions = validActions
+        returnActions = []
         for elems in state:
-            pointerval = 0
             if(elems == "F"):
+                returnActions.append("F")
                 continue
-            while pointerval != len(returnActions):
-                if(elems not in returnActions[pointerval] and len(returnActions[pointerval]) != 1):
-                    returnActions.remove(returnActions[pointerval])
-                    continue
-                pointerval += 1
+            returnActions.append("F" + elems)
 
         return returnActions
 
@@ -25,10 +21,9 @@ class WolfGoatCabbage(Problem):
             pointerval = 0
             while pointerval != len(returnActions):
                 if(elems.lower() in returnActions[pointerval]):
-                    returnActions.remove(returnActions[pointerval])
+                    returnActions.remove(returnActions[pointerval])                    
                     continue
                 pointerval += 1
-
         return returnActions
 
 
@@ -38,14 +33,13 @@ class WolfGoatCabbage(Problem):
     def result(self, state, action):
          ## Uppercase action indicates chosen character moves from left to right bank
         ## Lowercase action indicates chosen character moves from right to left bank
-        print("Before: ", state)
-        print("Action: ", action)
-        for elems in action[0]:
-            if(elems.lower()):
-                state.add(elems.upper())
-            if(elems.upper()):
-                state.discard(elems)
-        return state
+        curState = list(state)
+        for elems in action:
+            if(elems.islower()):
+                curState.append(elems.upper())
+            elif(elems.isupper()):
+                curState.remove(elems)
+        return tuple(curState)
     
     def actions(self, state):
         ## Uppercase values indicate entities are on the left bank
@@ -58,7 +52,6 @@ class WolfGoatCabbage(Problem):
 
         del validActions[bankSide[0]:bankSide[1]]
 
-        print(bankSide[2])
 
         if(bankSide[2] == False):
             return self.rightBankActions(state, validActions)
@@ -72,20 +65,7 @@ class WolfGoatCabbage(Problem):
  
 if __name__ == '__main__':
     wgc = WolfGoatCabbage()
-    test_state = {'F', 'W', 'C'}
-    test_action = ['FW']
-    test_state2 = {'W' , 'C'}
-    test_action2 = ['f']
-    #test 1
-    print(wgc.actions(test_state))
-    print("After", wgc.result(test_state, test_action))
-    #test 2
-    print(wgc.actions(test_state2))
-    print("After", wgc.result(test_state2, test_action2))
-
-    """
-    solution = depth_first_graph_search(wgc).solution()
-    print(solution)
     solution = breadth_first_graph_search(wgc).solution()
     print(solution)
-    """
+    solution = depth_first_graph_search(wgc).solution()
+    print(solution)
